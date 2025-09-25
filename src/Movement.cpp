f4f3a91e05df0d74c2c3fbdb7074c4c28cb04589
Fire-Cheese <sammycooly0.0@gmail.com>
2025-09-25 11:04:29 -0400

    added movements.h, and fixed a soon to be bug when turning in pseudocode and movements.cpp

--- src/Movement.cpp
+++ src/Movement.cpp
@@ -3,6 +3,7 @@
 #include "main.h"
 #include "Circuit.h"
 #include "DetectionState.h"
+#include "Movement.h"
 
 void moveforward(){
     if(bDangerCheck() == false){
@@ -17,10 +18,10 @@ void moveforward(){
 void turnRight(){
     
     //turn right using PID:
-        //go forward a bit (so rotation point is in the middle)
+        //reverse a bit (so rotation point is in the middle)
         //stop
         //turn left wheel forwards and right wheel backwards until faced right
-        //reverse a bit (so robot is perfectly in the center of the «square»)
+        //go forward a bit (so robot is perfectly in the center of the «square»)
     
     vUpdateRobotDirection(RIGHT);
     if(bDangerCheck() == true)
@@ -32,10 +33,10 @@ void turnRight(){
 void turnLeft(){
 
     //turn left using PID:
-        //go forward a bit (so rotation point is in the middle)
+        //reverse a bit (so rotation point is in the middle)
         //stop
         //turn right wheel forwards and left wheel backwards until faced left
-        //reverse a bit (so robot is perfectly in the center of the «square»)
+        //go forward a bit (so robot is perfectly in the center of the «square»)
 
     vUpdateRobotDirection(LEFT);
     if(bDangerCheck() == true)
