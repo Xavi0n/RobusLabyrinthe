39d0f33e3b59349fe003350a0a219e8850bef108
Xavi0n <xavchampoux@gmail.com>
2025-09-25 11:40:26 -0400

    Modification of logic and size of table to allow a more accurate depiction of the lines on the ground

--- include/Circuit.h
+++ include/Circuit.h
@@ -9,25 +9,37 @@
 #define LEFT 0
 #define RIGHT 1
 
-#define LEFT_COLUMN 1
-#define CENTER_COLUMN 2
-#define RIGHT_COLUMN 3
+//Table mapping the lines is flipped vs IRL, hence why the left column IRL is the right column in the table
+#define LEFT_COLUMN 5
+#define CENTER_COLUMN 3
+#define RIGHT_COLUMN 1
 
 // 1 means line present, 0 means no line. Each element of the table represents the edge
 // of a 50x50 cm square in the maze.
 // The first line of the table represents the start line of the maze, which means maze is flipped in the table vs reality.
-unsigned char uctWhereAreLines [11][4] = {
-  {0,0,0,0},
-  {0,0,0,0},
-  {0,0,0,0},
-  {0,0,0,0},
-  {0,0,0,0},
-  {0,0,0,0},
-  {0,0,0,0},
-  {0,0,0,0},
-  {0,0,0,0},
-  {0,0,0,0},
-  {0,0,0,0}
+unsigned char uctWhereAreLines [22][7] = {
+  {1,1,1,1,1,1,1},
+  {1,0,0,0,0,0,1},
+  {1,0,0,0,0,0,1},
+  {1,0,1,0,1,0,1},
+  {1,0,1,0,1,0,1},
+  {1,0,0,0,0,0,1},
+  {1,0,0,0,0,0,1},
+  {1,0,1,0,1,0,1},
+  {1,0,1,0,1,0,1},
+  {1,0,0,0,0,0,1},
+  {1,0,0,0,0,0,1},
+  {1,0,1,0,1,0,1},
+  {1,0,1,0,1,0,1},
+  {1,0,0,0,0,0,1},
+  {1,0,0,0,0,0,1},
+  {1,0,1,0,1,0,1},
+  {1,0,1,0,1,0,1},
+  {1,0,0,0,0,0,1},
+  {1,0,0,0,0,0,1},
+  {1,0,1,0,1,0,1},
+  {1,0,1,0,1,0,1},
+  {1,1,1,1,1,1,1}
 };
 
 //FUNCTION PROTOTYPES
@@ -36,4 +48,4 @@ void vUpdateRobotPosition();
 bool bGetNearbyLines(unsigned char ucRobotDirection, unsigned char ucWhereIsRobotX, unsigned char ucWhereIsRobotY);
 bool bDangerCheck ();
 
-#endif
+#endif // CIRCUIT_H
